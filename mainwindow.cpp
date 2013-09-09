fbc46ef9efacf47f042905712e530c5b77bff51b
Alan Garny <agarny@hellix.com>
2013-09-09 10:07:28 +0200

    Some minor cleaning up.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -52,12 +52,10 @@ MainWindow::MainWindow()
 
     QCheckBox *checkDrawFrames = new QCheckBox( toolBar );
     checkDrawFrames->setText( "Draw frames" );
-    checkDrawFrames->setChecked( true );
     toolBar->addWidget( checkDrawFrames );
 
     QCheckBox *checkColors = new QCheckBox( toolBar );
     checkColors->setText( "Colors" );
-    checkColors->setChecked( true );
     toolBar->addWidget( checkColors );
 
     addToolBar( toolBar );
