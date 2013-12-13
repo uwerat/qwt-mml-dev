da9d8704b0c12b7726895852dfba1087b335a726
Alan Garny <agarny@hellix.com>
2013-12-13 15:15:14 +0100

    Simplified QwtMmlDocument::setContent() to fix an issue with building using Qt 5.2.

--- qwt_mml_document.cpp
+++ qwt_mml_document.cpp
@@ -1246,23 +1246,9 @@ bool QwtMmlDocument::setContent(
 {
     clear();
 
-    QString prefix = "<?xml version=\"2.0\"?>\n";
-    prefix.append( mmlEntityTable.entities() );
-
-    int prefix_lines = 0;
-    for ( int i = 0; i < prefix.length(); ++i )
-    {
-        if ( prefix.at( i ) == '\n' )
-            ++prefix_lines;
-    }
-
     QDomDocument dom;
-    if ( !dom.setContent( prefix + text, false, errorMsg, errorLine, errorColumn ) )
-    {
-        if ( errorLine != 0 )
-            *errorLine -= prefix_lines;
+    if ( !dom.setContent( text, false, errorMsg, errorLine, errorColumn ) )
         return false;
-    }
 
     // we don't have access to line info from now on
     if ( errorLine != 0 ) *errorLine = -1;
@@ -1333,7 +1319,7 @@ bool QwtMmlDocument::insertChild( QwtMmlNode *parent, QwtMmlNode *new_node,
             n->m_next_sibling = new_node;
             new_node->m_previous_sibling = n;
         }
-        else 
+        else
         {
             parent->m_first_child = new_node;
         }
@@ -2156,7 +2142,7 @@ void QwtMmlNode::paintSymbol( QPainter *painter, qreal, qreal ) const
         painter->restore();
     }
 #else
-	Q_UNUSED( painter )
+    Q_UNUSED( painter )
 #endif
 }
 
